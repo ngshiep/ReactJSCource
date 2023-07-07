d9178c0f2605cbda2cf2699b3782db9ac2d42e8b
hieppnguyen <n.s.hiep@ttdesignco.com>
2023-07-07 16:31:19 +0700

    browser info

---
+++ 5.JSX-2/brower-info/src/components/BrowserInfo/index.jsx
@@ -0,0 +1,7 @@
+
+export default function BrowserInfo() {
+  return (
+    // eslint-disable-next-line react/no-unescaped-entities
+    <h4 style={{textAlign:'center', width:'100vw'}}>Browser's details: {navigator.userAgent}</h4>
+  )
+}
---
+++ 5.JSX-2/brower-info/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import BrowserInfo from './components/BrowserInfo/index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserInfo />
+  </React.StrictMode>,
+)
