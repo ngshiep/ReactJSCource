ca0b0bf0665bee4705c622fce4c04dadac6e2cc8
hieppnguyen <n.s.hiep@ttdesignco.com>
2023-07-07 17:03:20 +0700

    sign-in-form

---
+++ 5.JSX-2/sign-in-form/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import SignInForm from './components/SignInForm/index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SignInForm />
+  </React.StrictMode>,
+)
